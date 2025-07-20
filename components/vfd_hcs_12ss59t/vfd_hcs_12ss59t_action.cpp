82e6daf9971970a3d0dbfb2ceeb23f86cf223073
Bas Bieling <bas@scrumble.nl>
2025-07-20 19:21:33 +0200

    fixes

--- components/vfd_hcs_12ss59t/vfd_hcs_12ss59t_action.cpp
+++ components/vfd_hcs_12ss59t/vfd_hcs_12ss59t_action.cpp
@@ -1,7 +1,22 @@
 #include "vfd_hcs_12ss59t_action.h"
+#include "vfd_hcs_12ss59t.h"
 
 namespace esphome {
 namespace vfd_hcs_12ss59t {
-// Nothing needed here, all logic is in header inline
+
+VFDWriteAction::VFDWriteAction(VFDHCS12SS59T *parent, std::string text)
+    : parent_(parent), text_(std::move(text)) {}
+
+void VFDWriteAction::play() {
+  parent_->write(text_.c_str());
+}
+
+VFDClearAction::VFDClearAction(VFDHCS12SS59T *parent)
+    : parent_(parent) {}
+
+void VFDClearAction::play() {
+  parent_->clear();
+}
+
 }  // namespace vfd_hcs_12ss59t
 }  // namespace esphome
--- components/vfd_hcs_12ss59t/vfd_hcs_12ss59t_action.h
+++ components/vfd_hcs_12ss59t/vfd_hcs_12ss59t_action.h
@@ -12,8 +12,8 @@ class VFDHCS12SS59T;  // Forward declaration
 
 class VFDWriteAction : public esphome::Action<> {
  public:
-  VFDWriteAction(VFDHCS12SS59T *parent, std::string text) : parent_(parent), text_(std::move(text)) {}
-  void play() override { parent_->write(text_.c_str()); }
+  VFDWriteAction(VFDHCS12SS59T *parent, std::string text);
+  void play() override;
 
  protected:
   VFDHCS12SS59T *parent_;
@@ -22,8 +22,8 @@ class VFDWriteAction : public esphome::Action<> {
 
 class VFDClearAction : public esphome::Action<> {
  public:
-  explicit VFDClearAction(VFDHCS12SS59T *parent) : parent_(parent) {}
-  void play() override { parent_->clear(); }
+  explicit VFDClearAction(VFDHCS12SS59T *parent);
+  void play() override;
 
  protected:
   VFDHCS12SS59T *parent_;
