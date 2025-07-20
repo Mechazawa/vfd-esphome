4eb0a7868faf099f69dc39c713b458623d169122
Bas Bieling <bas@scrumble.nl>
2025-07-20 19:14:59 +0200

    fixes

--- components/vfd_hcs_12ss59t/vfd_hcs_12ss59t_action.h
+++ components/vfd_hcs_12ss59t/vfd_hcs_12ss59t_action.h
@@ -1,27 +1,29 @@
 
 #pragma once
 
-#include "esphome/components/vfd_hcs_12ss59t/vfd_hcs_12ss59t.h"
+
 #include "esphome/core/automation.h"
 #include <string>
 
 namespace esphome {
 namespace vfd_hcs_12ss59t {
 
-class VFDWriteAction : public esphome::Action<void> {
+class VFDHCS12SS59T;  // Forward declaration
+
+class VFDWriteAction : public esphome::Action<> {
  public:
   VFDWriteAction(VFDHCS12SS59T *parent, std::string text) : parent_(parent), text_(std::move(text)) {}
-  void play(esphome::ActionInfo &info) override { parent_->write(text_.c_str()); }
+  void play() override { parent_->write(text_.c_str()); }
 
  protected:
   VFDHCS12SS59T *parent_;
   std::string text_;
 };
 
-class VFDClearAction : public esphome::Action<void> {
+class VFDClearAction : public esphome::Action<> {
  public:
   explicit VFDClearAction(VFDHCS12SS59T *parent) : parent_(parent) {}
-  void play(esphome::ActionInfo &info) override { parent_->clear(); }
+  void play() override { parent_->clear(); }
 
  protected:
   VFDHCS12SS59T *parent_;
